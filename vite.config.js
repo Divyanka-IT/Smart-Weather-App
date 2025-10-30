b8996395f448e17a9ba216db456219ab8fe369a3
Divyanka-IT <sangitamth73@gmail.com>
2025-10-30 19:08:08 +0530

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Smart-Weather-App/',  // 👈 Must match your repo name exactly
+})
